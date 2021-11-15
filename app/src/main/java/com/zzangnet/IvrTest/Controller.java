3c39e1f349ee22e4f0f779f3bf6053e310e41977
justyh <justyh@naver.com>
2021-11-15 18:57:37 +0900

    first source1

---
+++ app/src/main/java/com/zzangnet/IvrTest/Controller.java
@@ -0,0 +1,14 @@
+package com.zzangnet.IvrTest;
+
+import java.util.concurrent.BlockingDeque;
+
+public class Controller
+{
+    private boolean started;
+
+    //private RecordTask recordTask;
+    //private RecognizerTask recognizerTask;
+    private MainActivity mainActivity;
+    //BlockingDeque<DataBlock> blockingQueue;
+
+}
