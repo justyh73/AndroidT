ff9fb9e80b7dc44dc65bdeca85919c14a0d275a7
justyh <justyh@naver.com>
2021-11-16 18:58:37 +0900

    두번째 작업 파일 입니다. 2/*

--- app/src/main/java/com/zzangnet/IvrTest/Controller.java
+++ app/src/main/java/com/zzangnet/IvrTest/Controller.java
@@ -1,6 +1,7 @@
 package com.zzangnet.IvrTest;
 
 import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.LinkedBlockingDeque;
 
 public class Controller
 {
@@ -10,5 +11,39 @@ public class Controller
     private RecognizerTask recognizerTask;
     private MainActivity mainActivity;
     BlockingDeque<DataBlock> blockingQueue;
+
+    private  Character lastValue;
+
+    public Controller(MainActivity mainActivity)
+    {
+        this.mainActivity = mainActivity;
+    }
+
+    public void changeState()
+    {
+        if (started == false)
+        {
+            lastValue = ' ';
+            blockingQueue = new LinkedBlockingDeque<DataBlock>();
+            mainActivity.start();
+            recordTask = new RecordTask(this,blockingQueue);
+            recognizerTask = new RecognizerTask(this, blockingQueue);
+
+            recordTask.execute();
+            recognizerTask.execute();
+
+            started = true;
+        } else {
+
+            mainActivity.stop();
+
+            recognizerTask.cancel(true);
+            recordTask.cancel(true);
+
+            started = false;
+        }
+    }
+
+
 
 }
